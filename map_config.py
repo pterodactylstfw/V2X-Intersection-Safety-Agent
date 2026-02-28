0108522d15494aba4eaccc2bf0ac7865a2b29c19
Bianca Diana <andaras.bianca@gmail.com>
2026-02-28 22:58:53 +0200

    modified map config

--- map_config.py
+++ map_config.py
@@ -4,36 +4,35 @@
 
 nodes = {
     # --- MARGINI (Punctele de Spawn / Ieșire nu se schimbă) ---
-    "W_START": (0, 670),
-    "W_END": (0, 630),
-    "E_START": (1500, 630),
-    "E_END": (1500, 670),
+    "W_START": (0, 675),
+    "W_END": (0, 635),
+    "E_START": (1500, 635),
+    "E_END": (1500, 675),
     "S1_START": (420, 800),
     "S1_END": (380, 800),
     "S2_START": (1120, 800),
     "S2_END": (1080, 800),
     "NW_START": (0, 100),
-    "NW_END": (0, 50),
+    "NW_END": (0, 70),
     "NE_ONEWAY_START": (1000, 0),
     # --- INTERSECȚIA 1 (4 noduri centrale) ---
-    "I1_NW": (380, 630),
-    "I1_NE": (420, 630),
-    "I1_SE": (420, 670),
-    "I1_SW": (380, 670),
+    "I1_NW": (380, 635),
+    "I1_NE": (420, 635),
+    "I1_SE": (420, 675),
+    "I1_SW": (380, 675),
     # --- INTERSECȚIA 2 (4 noduri centrale) ---
-    "I2_NW": (1080, 630),
-    "I2_NE": (1120, 630),
-    "I2_SE": (1120, 670),
-    "I2_SW": (1080, 670),
+    "I2_NW": (1080, 635),
+    "I2_NE": (1120, 635),
+    "I2_SE": (1120, 675),
+    "I2_SW": (1080, 675),
     # --- INTERSECȚIA 3 (4 noduri centrale) ---
-    "I3_NW": (380, 280),
+    "I3_NW": (380, 290),
     "I3_NE": (420, 280),
-    "I3_SE": (420, 320),
+    "I3_SE": (420, 310),
     "I3_SW": (380, 320),
     # --- INTERSECȚIA 4 (Merge Points - rămâne la fel de simplu) ---
-    "I4_IN": (840, 350),  # Punctul de intrare dinspre Nord-Est
     "MERGE_UP": (770, 455),  # Mijlocul matematic perfect între I2_NE și I3_NE
-    "MERGE_DOWN": (750, 475),
+    "MERGE_DOWN": (760, 475),
 }
 
 edges = [
@@ -66,9 +65,8 @@ edges = [
     ("I2_NE", "MERGE_UP", 1),  # Din I2 urcă spre I3
     ("MERGE_UP", "I3_NE", 1),
     # --- 3. SENS UNIC I4 ---
-    ("NE_ONEWAY_START", "I4_IN", 1),
-    ("I4_IN", "MERGE_UP", 1),  # Se varsă spre I3
-    ("I4_IN", "MERGE_DOWN", 1),  # Se varsă spre I2
+    ("NE_ONEWAY_START", "MERGE_UP", 1),
+    ("NE_ONEWAY_START", "MERGE_DOWN", 1),  # Se varsă spre I2
     # --- 4. LEGAREA INTERNĂ A INTERSECȚIILOR (Pătratul Perpendicular) ---
     # Mașinile circulă Counter-Clockwise în interior (regula de prioritate dreapta)
     # I1
